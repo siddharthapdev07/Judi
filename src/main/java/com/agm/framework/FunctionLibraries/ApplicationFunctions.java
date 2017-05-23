1d2ffc694e2d7ea54327f0eb797740918beeaeaa
Suresh Mylam <Suresh Mylam@192.168.102.0>
2017-05-23 12:17:07 -0400

    Updated library and removed few unwanted results

--- src/main/java/com/agm/framework/FunctionLibraries/ApplicationFunctions.java
+++ src/main/java/com/agm/framework/FunctionLibraries/ApplicationFunctions.java
@@ -8,67 +8,94 @@ import com.relevantcodes.extentreports.LogStatus;
 
 public class ApplicationFunctions {
 	// Creating singleton
-		private static ApplicationFunctions objApf = null;
+	private static ApplicationFunctions objApf = null;
 
-		public WebDriver driver = null;
-		public ExtentTest test = null;
-		WebElement element ;
-		private ApplicationFunctions() {
-			
-		}
+	public WebDriver driver = null;
+	public ExtentTest test = null;
+	WebElement element;
 
-		public static ApplicationFunctions getInstance() {
-			if (objApf == null)
-				objApf = new ApplicationFunctions();
-			return objApf;
-		}
+	private ApplicationFunctions() {
 
-		public void init(ExtentTest test) {
-			this.test = test;
-		}
-	
-		public void init(WebDriver driver) {
-			this.driver = driver;
-		}
-	
-	public void funLoginApplication()
-	{
-		try{
-			CommonFunctions.getInstance().getElement(driver, "judi.test.login.username").sendKeys(Initializer.getInstance().GetValue("app.test.test05.username"));
-			test.log(LogStatus.PASS, "Userid Entered successfully","");
-		}catch(Exception e){
-			CommonFunctions.getInstance().funLog("Issue identifying the object - UserName" + e.getMessage());
-			test.log(LogStatus.FAIL, "Exception in identifying the UserName field","");
-		}
-		try{		
-			CommonFunctions.getInstance().getElement(driver, "judi.test.login.password").sendKeys(Initializer.getInstance().GetValue("app.test.test05.password"));
-			test.log(LogStatus.PASS, "Password field verification","");
-		}catch(Exception e){
-			CommonFunctions.getInstance().funLog("Issue identifying the object - Password" + e.getMessage());
-			test.log(LogStatus.FAIL, "Exception in identifying the password field","");
-		}
-		try{		
-			CommonFunctions.getInstance().getElement(driver, "judi.test.login.login").click();
-			test.log(LogStatus.PASS, "Login Button is clicked successfully","");
-			CommonFunctions.getInstance().funWait(15);
-		}catch(Exception e){
-			CommonFunctions.getInstance().funLog("Issue identifying the object - LoginButton" + e.getMessage());
-			test.log(LogStatus.FAIL, "Exception in clicking the Login button",test.addScreenCapture(CommonFunctions.getInstance().funTakeScreenshot(Thread.currentThread().getStackTrace()[1].getMethodName())));
-		}
-		//Login Validation
-//		try{		
-//			if(CommonFunctions.getInstance().getElement(driver, "judi.test.home.logout").isDisplayed()==true){
-//				test.log(LogStatus.PASS, "Login to application is successful",test.addScreenCapture(CommonFunctions.getInstance().funTakeScreenshot(Thread.currentThread().getStackTrace()[1].getMethodName())));
-//			}else{
-//				test.log(LogStatus.FAIL, "Login to application is Failed","");
-//			}
-//		}catch(Exception e){
-//			CommonFunctions.getInstance().funLog("Issue identifying the object - LogOut" + e.getMessage());
-//			test.log(LogStatus.FAIL, "Issue identifying the object - LogOut",test.addScreenCapture(CommonFunctions.getInstance().funTakeScreenshot(Thread.currentThread().getStackTrace()[1].getMethodName())));
-//		}
-		CommonFunctions.getInstance().funElementValidate(CommonFunctions.getInstance().getElement(driver, "judi.test.home.logout"),"ISPRESENT", "Validating LogOut element : ", true, true);
-		
 	}
-	
+
+	public static ApplicationFunctions getInstance() {
+		if (objApf == null)
+			objApf = new ApplicationFunctions();
+		return objApf;
+	}
+
+	public void init(ExtentTest test) {
+		this.test = test;
+	}
+
+	public void init(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void funLoginApplication() {
+		try {
+			CommonFunctions
+					.getInstance()
+					.getElement(driver, "judi.test.login.username")
+					.sendKeys(
+							Initializer.getInstance().GetValue(
+									"app.test.test05.username"));
+			test.log(LogStatus.PASS, "Userid Entered successfully", "");
+		} catch (Exception e) {
+			CommonFunctions.getInstance().funLog(
+					"Issue identifying the object - UserName" + e.getMessage());
+			test.log(LogStatus.FAIL,
+					"Exception in identifying the UserName field", "");
+		}
+		try {
+			CommonFunctions
+					.getInstance()
+					.getElement(driver, "judi.test.login.password")
+					.sendKeys(
+							Initializer.getInstance().GetValue(
+									"app.test.test05.password"));
+			test.log(LogStatus.PASS, "Password field verification", "");
+		} catch (Exception e) {
+			CommonFunctions.getInstance().funLog(
+					"Issue identifying the object - Password" + e.getMessage());
+			test.log(LogStatus.FAIL,
+					"Exception in identifying the password field", "");
+		}
+		try {
+			CommonFunctions.getInstance()
+					.getElement(driver, "judi.test.login.login").click();
+			test.log(LogStatus.PASS, "Login Button is clicked successfully", "");
+			CommonFunctions.getInstance().funWait(15);
+		} catch (Exception e) {
+			CommonFunctions.getInstance().funLog(
+					"Issue identifying the object - LoginButton"
+							+ e.getMessage());
+			test.log(LogStatus.FAIL, "Exception in clicking the Login button",
+					test.addScreenCapture(CommonFunctions.getInstance()
+							.funTakeScreenshot(
+									Thread.currentThread().getStackTrace()[1]
+											.getMethodName())));
+		}
+		// Login Validation
+		// try{
+		// if(CommonFunctions.getInstance().getElement(driver,
+		// "judi.test.home.logout").isDisplayed()==true){
+		// test.log(LogStatus.PASS,
+		// "Login to application is successful",test.addScreenCapture(CommonFunctions.getInstance().funTakeScreenshot(Thread.currentThread().getStackTrace()[1].getMethodName())));
+		// }else{
+		// test.log(LogStatus.FAIL, "Login to application is Failed","");
+		// }
+		// }catch(Exception e){
+		// CommonFunctions.getInstance().funLog("Issue identifying the object - LogOut"
+		// + e.getMessage());
+		// test.log(LogStatus.FAIL,
+		// "Issue identifying the object - LogOut",test.addScreenCapture(CommonFunctions.getInstance().funTakeScreenshot(Thread.currentThread().getStackTrace()[1].getMethodName())));
+		// }
+		CommonFunctions.getInstance().funElementValidate(
+				CommonFunctions.getInstance().getElement(driver,
+						"judi.test.home.logout"), "ISPRESENT",
+				"Validating LogOut element : ", true, true);
+
+	}
 
 }
