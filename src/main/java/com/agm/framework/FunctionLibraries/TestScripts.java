422a94d09891a9ecf3e9ce44cf59122f2bf37e2a
Suresh Mylam <Suresh Mylam@192.168.102.0>
2017-06-01 16:12:09 -0400

    Added new Library for testscript construction

---
+++ src/main/java/com/agm/framework/FunctionLibraries/TestScripts.java
@@ -0,0 +1,30 @@
+package com.agm.framework.FunctionLibraries;
+
+import com.agm.framework.helpers.Stage;
+
+public class TestScripts {
+	
+	private static TestScripts objTs = null;
+	
+	public static TestScripts getInstance() {
+		if (objTs == null)
+			objTs = new TestScripts();
+		return objTs;
+	}
+	
+
+	ApplicationFunctions applicationFunctions = ApplicationFunctions
+			.getInstance();
+	CommonFunctions commonFunctions = CommonFunctions.getInstance();
+
+	public void DemoTest() {
+		// Login Application
+		applicationFunctions.funLoginApplication();		
+		//Select Test Trail
+		applicationFunctions.funSelectTestTrial("AutomationTestTrial");	
+		//Invite Users
+		applicationFunctions.funInviteUser("autotest_freetodelete3@mx-intr.agmednet.net", "Event Coordinator");
+		//Register users
+		applicationFunctions.funRegistration("autotest_freetodelete3@mx-intr.agmednet.net");
+	}
+}
