d02b73b2372769be2f4d8f4b336ec999b54a1a60
Suresh Mylam <Suresh Mylam@192.168.102.0>
2017-06-16 11:43:23 -0400

    This is not required for testing, so commented all the code

--- src/main/java/com/agm/framework/FunctionLibraries/TestData.java
+++ src/main/java/com/agm/framework/FunctionLibraries/TestData.java
@@ -1,67 +1,69 @@
-package com.agm.framework.FunctionLibraries;
-
-import com.agm.framework.helpers.Stage;
-import com.relevantcodes.extentreports.ExtentTest;
-
-public class TestData {
-
-	private static TestData objTd = null;
-	public ExtentTest test = null;
-
-	public static TestData getInstance() {
-		if (objTd == null)
-			objTd = new TestData();
-		return objTd;
-	}
-
-	int iCaseID;
-	int iTestID;
-	int iRunID;
-
-	public void init(ExtentTest test) {
-		this.test = test;
-	}
-	
-	/*
-	 * ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-	 * Function Name : funLoadTestDetailsFromTestRail() Description : This
-	 * function will Load the TestID,Case ID and Run ID for the test case Author
-	 * : Suresh Kumar,Mylam Date : 17 May 2017 Parameter : strTestCaseName :
-	 * Provide the test case name
-	 * ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-	 */
-	public void funLoadTestData(String strTestCaseName) {
-		try {
-			switch (strTestCaseName.toUpperCase()) {
-			case "DEMOTEST":
-				iCaseID = 541421;
-				iTestID = 1079709;
-				iRunID = 2081;				
-				break;
-			case "DEMOTEST2":
-				iCaseID = 541421;
-				iTestID = 1079709;
-				iRunID = 2081;
-				break;
-			case "DEMOTEST3":
-				iCaseID = 541421;
-				iTestID = 1079709;
-				iRunID = 2081;
-				break;
-			default:
-				CommonFunctions.getInstance()
-						.funLog("Issue on identifying the test case - Please add New Case for the running Test");
-
-			}
-			Stage.getInstance().setCaseID(iCaseID);
-			Stage.getInstance().setTestID(iTestID);
-			Stage.getInstance().setRunID(iRunID);
-
-		} catch (Exception e) {
-			CommonFunctions.getInstance()
-					.funLog("Exception occured while setting test details. Exception : "
-							+ e.getMessage());
-		}
-	}
-
-}
+//package com.agm.framework.FunctionLibraries;
+//
+//import com.agm.framework.helpers.Stage;
+//import com.relevantcodes.extentreports.ExtentTest;
+//
+//public class TestData {
+//
+//	private static TestData objTd = null;
+//	public ExtentTest test = null;
+//
+//	public static TestData getInstance() {
+//		if (objTd == null)
+//			objTd = new TestData();
+//		return objTd;
+//	}
+//
+//	int iCaseID;
+//	int iTestID;
+//	int iRunID;
+//
+//	public void init(ExtentTest test) {
+//		this.test = test;
+//	}
+//
+//	/*
+//	 * ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+//	 * Function Name : funLoadTestDetailsFromTestRail() Description : This
+//	 * function will Load the TestID,Case ID and Run ID for the test case Author
+//	 * : Suresh Kumar,Mylam Date : 17 May 2017 Parameter : strTestCaseName :
+//	 * Provide the test case name
+//	 * ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+//	 */
+//	public void funLoadTestData(String strTestCaseName) {
+//		try {
+//			switch (strTestCaseName.toUpperCase()) {
+//			case "Validate_TrialAdmin_TrialsSitesSubjectsUsers":
+//				iCaseID = 541421;
+//				iTestID = 1079709;
+//				iRunID = 2081;
+//				// strTrial = "Judi_Demo";
+//				break;
+//			case "DEMOTEST2":
+//				iCaseID = 541421;
+//				iTestID = 1079709;
+//				iRunID = 2081;
+//				break;
+//			case "DEMOTEST3":
+//				iCaseID = 541421;
+//				iTestID = 1079709;
+//				iRunID = 2081;
+//				break;
+//			default:
+//				CommonFunctions
+//						.getInstance()
+//						.funLog("Issue on identifying the test case - Please add New Case for the running Test");
+//
+//			}
+//			Stage.getInstance().setCaseID(iCaseID);
+//			Stage.getInstance().setTestID(iTestID);
+//			Stage.getInstance().setRunID(iRunID);
+//
+//		} catch (Exception e) {
+//			CommonFunctions.getInstance().funLog(
+//					"Exception occured while setting test details. Exception : "
+//							+ e.getMessage());
+//		}
+//	}
+//
+//}
