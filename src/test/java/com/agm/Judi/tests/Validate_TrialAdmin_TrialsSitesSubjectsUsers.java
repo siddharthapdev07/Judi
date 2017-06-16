ba14b129bb2672229ee76a0acbea924907a7512c
Suresh Mylam <Suresh Mylam@192.168.102.0>
2017-06-16 11:42:40 -0400

    updated

--- src/test/java/com/agm/Judi/tests/Validate_TrialAdmin_TrialsSitesSubjectsUsers.java
+++ src/test/java/com/agm/Judi/tests/Validate_TrialAdmin_TrialsSitesSubjectsUsers.java
@@ -1,6 +1,7 @@
 package com.agm.Judi.tests;
 
 import org.testng.ITestResult;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
@@ -11,27 +12,28 @@ import com.agm.framework.FunctionLibraries.TestScripts;
 public class Validate_TrialAdmin_TrialsSitesSubjectsUsers {
 
 	public ITestResult result;
-	
+
 	CommonFunctions commonFunctions = CommonFunctions.getInstance();
 	TestScripts testScripts = TestScripts.getInstance();
-	
+
 	@BeforeMethod
-	public void beforeMethod() {		
-		commonFunctions.funBeforeTest();		
+	public void beforeMethod() {
+		commonFunctions.funBeforeTest();
 	}
+		
 
 	@Test
-	public void Test() {	
-		//Test Script
-		testScripts.Validate_TrialAdmin_TrialsSitesSubjectsUsers();	
-		
-		//Finalizing the reports
+	public void Test() {
+		// Test Script
+		testScripts.Validate_TrialAdmin_TrialsSitesSubjectsUsers();
+
+		// Finalizing the reports
 		commonFunctions.funFinalizeResults();
-		
+
 	}
 
 	@AfterMethod
-	public void afterMethod(ITestResult result) {		
-		commonFunctions.funAfterTest(result);		
+	public void afterMethod(ITestResult result) {
+		commonFunctions.funAfterTest(result);
 	}
 }
