5f9cde06a3d17ea98478de3fff9eb7d0af5945b2
Suresh Mylam <Suresh Mylam@DESKTOP-8T83S39>
2017-06-30 14:22:19 -0400

    Updated

--- src/main/java/com/agm/framework/FunctionLibraries/TestScripts.java
+++ src/main/java/com/agm/framework/FunctionLibraries/TestScripts.java
@@ -43,25 +43,25 @@ public class TestScripts {
 		// Login Application
 		ApplicationFunctions.getInstance().funLoginApplication();
 		// Navigate to Trial Administration page and Select Trial
-//		ApplicationFunctions.getInstance().funSelectTrial(strTrial);
-//		// Select Test Trail
-//		ApplicationFunctions.getInstance().funTrialAdmin_Trials(strTrial);
-//		// add site
-//		ApplicationFunctions.getInstance().funTrialAdmin_Sites("ADDSITE",
-//				strSiteID);
-//		// add subject
-//		ApplicationFunctions.getInstance().funTrialAdmin_Subjects("ADDSUBJECT",
-//				"");
-//		// add multiple sites
-//		ApplicationFunctions.getInstance().funTrialAdmin_Subjects(
-//				"ADDMULTIPLESUBJECTS", "");
-//		// add multiple sites
-//		ApplicationFunctions.getInstance().funTrialAdmin_Sites(
-//				"ADDMULTIPLESITES", "");
-//		// //Invite Users
-//		ApplicationFunctions.getInstance().funInviteUser(
-//				"autotest_freetodelete3@mx-intr.agmednet.net",
-//				"Event Coordinator");
+		ApplicationFunctions.getInstance().funSelectTrial(strTrial);
+		// Select Test Trail
+		ApplicationFunctions.getInstance().funTrialAdmin_Trials(strTrial);
+		// add site
+		ApplicationFunctions.getInstance().funTrialAdmin_Sites("ADDSITE",
+				strSiteID);
+		// add subject
+		ApplicationFunctions.getInstance().funTrialAdmin_Subjects("ADDSUBJECT",
+				"");
+		// add multiple sites
+		ApplicationFunctions.getInstance().funTrialAdmin_Subjects(
+				"ADDMULTIPLESUBJECTS", "");
+		// add multiple sites
+		ApplicationFunctions.getInstance().funTrialAdmin_Sites(
+				"ADDMULTIPLESITES", "");
+		// //Invite Users
+		ApplicationFunctions.getInstance().funInviteUser(
+				"autotest_freetodelete3@mx-intr.agmednet.net",
+				"Event Coordinator");
 		// // Register users
 		// ApplicationFunctions.getInstance().funRegistration(
 		// "autotest_freetodelete3@mx-intr.agmednet.net");
