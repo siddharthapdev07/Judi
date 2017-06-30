73e6f5834e500cfd5939afc4213302fa20518e98
Suresh Mylam <Suresh Mylam@DESKTOP-8T83S39>
2017-06-30 13:51:15 -0400

    updated

--- src/main/java/com/agm/framework/FunctionLibraries/CommonFunctions.java
+++ src/main/java/com/agm/framework/FunctionLibraries/CommonFunctions.java
@@ -316,9 +316,10 @@ public class CommonFunctions {
 			strFileName = strFileName.replace(" ", "");
 			ImageIO.write(image, "jpg", new File(Initializer.getInstance()
 					.GetValue("java.error.path") + strFileName));
-			strFileName = new File(Initializer.getInstance().GetValue(
-					"java.error.path")
-					+ strFileName).getAbsolutePath();
+//			strFileName = new File(Initializer.getInstance().GetValue(
+//					"java.error.path")
+//					+ strFileName).getAbsolutePath();
+			strFileName = "../Images/"+strFileName;
 
 		} catch (Exception e) {
 			funLog("Issue on taking snapshot. Exception : " + e.getMessage());
